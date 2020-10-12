42be39b831324d40d1eaac51e8bb40ed9f2eca6e
Anthony Carrillo <acarrillorj@gmail.com>
2020-10-12 18:26:14 -0500

    push heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spotiapp/browser'));
+app.use(express.static(__dirname + '/dist/spotiapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spotiapp/browser/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spotiapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
